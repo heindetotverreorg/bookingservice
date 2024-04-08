9081d96afcda5aec96e5c0bcf324e3dfb8c56172
Matthias Poortvliet <matthias.poortvliet@kpn.com>
2024-04-08 13:49:01 +0200

    updated timeout time

--- server/crawl-web-page/index.js
+++ server/crawl-web-page/index.js
@@ -9,7 +9,7 @@ const {
 
 const init = async (page, freshBrowser, url) => {
     browser = freshBrowser
-    page.setDefaultTimeout(1000);
+    page.setDefaultTimeout(2000);
     await page.goto(url);
 }
 
